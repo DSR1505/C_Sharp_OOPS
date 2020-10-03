8a57b6bf855ee041dc773fea7e76ff937c03847d
Deependra Singh Rajput <34051384+DSR1505@users.noreply.github.com>
2020-10-03 20:07:10 +0530

    Update final-stage2-q2-t2.cs

--- final-stage2-q2-t2.cs
+++ final-stage2-q2-t2.cs
@@ -5,45 +5,45 @@ using System.Linq;
 
 class Customer
 {
-Public string cid,cname,city;
-Public int age;
+	public string cid, cname, city;
+	public int age;
 }
-class Shop 
+class Shop
 {
-	public string sid,sname;
-	public List<Customer> Normal_Customers=new List<Customer>();
-	public List<Customer> Premeium_Customers=new List<Customer>();
+	public string sid, sname;
+	public List<Customer> Normal_Customers = new List<Customer>();
+	public List<Customer> Premium_Customers = new List<Customer>();
 	//Constructors
-	
+
 	public void AddCustomer(Customer c, string type)
 	{
-	if(type==”Normal”)
+		if (type == "Normal")
 		Normal_Customers.Add(c);
-	else if(type==”Premium”)
-		Premeium_Customers.Add(c);
+	else if (type == "Premium")
+		Premium_Customers.Add(c);
 	}
-	
-	public bool RemoveCustomer(string  id)
-	{
-		bool flag1=false,   flag2=false;
-		Customer c1 = Normal_Customers.Find( x => x.cid == id);
-		if(c1 != null)
-		{
-			Normal_Customers.Remove(c1) ;
-			Flag1= true ;
-		}
-		Customer c2 = Premium_Customers.Find( x => x.cid == id);
-		if(c2 != null)
-		{
-			Premium_Customers.Remove(c2) ;
-			Flag2= true ;
-		}	
 
-		if( flag1 == true  | flag2 == true)
+	public bool RemoveCustomer(string id)
+	{
+		bool flag1 = false, flag2 = false;
+		Customer c1 = Normal_Customers.Find(x => x.cid == id);
+		if (c1 != null)
+		{
+			Normal_Customers.Remove(c1);
+			flag1 = true;
+		}
+		Customer c2 = Premium_Customers.Find(x => x.cid == id);
+		if (c2 != null)
+		{
+			Premium_Customers.Remove(c2);
+			flag2 = true;
+		}
+
+		if (flag1 == true | flag2 == true)
 		{
 			return true;
 		}
-		else 
-			return false ;
+		else
+			return false;
 	}
 }
